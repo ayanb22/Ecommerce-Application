469ace2f59b7127c6ee082ca2b3f115c6717d4f8
ayanb22 <ayanb2227@gmail.com>
2026-09-25 19:04:44 +0530

    corrected

--- backend/store/models.py
+++ backend/store/models.py
@@ -4,13 +4,13 @@ from decimal import Decimal
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)  #whats slug
+    slug = models.SlugField(unique=True)  
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)   #whats cascade
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)   
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='products/',blank=True, null= True)
--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -19,6 +19,7 @@ class CartItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name', read_only=True)
     product_price = serializers.DecimalField(source='product.dynamic_price', max_digits=10, decimal_places=2, read_only=True)
     product_image = serializers.ImageField(source='product.image', read_only=True)
+    subtotal = serializers.DecimalField(read_only=True, max_digits=10, decimal_places=2)
     class Meta:
         model = CartItem
         fields = '__all__'
